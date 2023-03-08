7b13db5fb35dae6e0e27276b39bb7876e08e3c59
tima <litin.lara74mail.ru@gmail.com>
2023-03-08 22:33:31 +0300

    middle-finish

--- hz/dog/dog/settings.py
+++ hz/dog/dog/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corm.apps.CormConfig'
+    'corm.apps.CormConfig',
+    'registration.apps.RegistrationConfig'
 ]
 
 MIDDLEWARE = [
